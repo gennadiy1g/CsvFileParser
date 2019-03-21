fc639dfedb79405d6b3878c39710252a9db5334b
Gennadiy <gennadiy1g@gmx.com>
2019-03-21 14:58:34 -0400

    Introduced std::vector<std::thread> threads.

--- CsvFileParser.cpp
+++ CsvFileParser.cpp
@@ -1,6 +1,7 @@
 #include "CsvFileParser.h"
 #include <string_view>
 #include <thread>
+#include <vector>
 
 CsvFileParser::CsvFileParser(std::wstring_view sourceFile)
 {
@@ -8,11 +9,22 @@ CsvFileParser::CsvFileParser(std::wstring_view sourceFile)
 
 void CsvFileParser::parse(wchar_t separator, wchar_t qoute, wchar_t escape, unsigned int numThreads_)
 {
-    unsigned int numThreads;
+    int numThreads;
     if (numThreads_ == 0) {
         numThreads = std::thread::hardware_concurrency();
     } else {
         numThreads = numThreads_;
+    }
+
+    // Launch threads
+    std::vector<std::thread> threads(numThreads);
+    for (auto& t : threads) {
+        t = std::thread{ &CsvFileParser::parseBuffer, this };
+    }
+
+    // Join on all threads
+    for (auto& t : threads) {
+        t.join();
     }
 }
 
