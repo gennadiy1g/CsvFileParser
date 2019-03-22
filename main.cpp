497621d49f37804ecf0adb1a27b518a85f8120da
Gennadiy <gennadiy1g@gmx.com>
2019-03-22 17:57:35 -0400

    Replaced logger_mt with severity_logger_mt<trivial::severity_level>.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/file.hpp>
 
@@ -10,8 +15,9 @@
 namespace logging = boost::log;
 namespace src = boost::log::sources;
 namespace keywords = boost::log::keywords;
+namespace trivial = boost::log::trivial;
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(my_logger, src::logger_mt)
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<trivial::severity_level>)
 
 void initLogging()
 {
@@ -24,8 +30,8 @@ void initLogging()
 int main(int argc, char** argv)
 {
     initLogging();
-    src::logger_mt& lg = my_logger::get();
-    BOOST_LOG(lg) << "Greetings from the global logger!";
+    src::severity_logger_mt<trivial::severity_level>& slg = gLogger::get();
+    BOOST_LOG_SEV(slg, trivial::trace) << "Greetings from the global logger!";
 
     unsigned int n = std::thread::hardware_concurrency();
     std::cout << n << " concurrent threads are supported.\n";
