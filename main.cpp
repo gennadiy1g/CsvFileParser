f10387064aba07e2ed5300afbd29c022e884ac9b
Gennadiy <gennadiy1g@gmx.com>
2019-03-22 17:35:53 -0400

    Logging seems to be working.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
 #include <iostream>
 #include <thread>
 
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace keywords = boost::log::keywords;
+
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(my_logger, src::logger_mt)
+
+void initLogging()
+{
+    logging::add_file_log(
+        keywords::file_name = "trace.log",
+        keywords::format = "[%TimeStamp%]: %Message%");
+    logging::add_common_attributes();
+}
+
 int main(int argc, char** argv)
 {
+    initLogging();
+    src::logger_mt& lg = my_logger::get();
+    BOOST_LOG(lg) << "Greetings from the global logger!";
+
     unsigned int n = std::thread::hardware_concurrency();
     std::cout << n << " concurrent threads are supported.\n";
     return 0;
