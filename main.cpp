6365745429e25152c722dfb9b2605bb24d5f311d
Gennadiy <gennadiy1g@gmx.com>
2019-03-25 13:46:54 -0400

    Renamed namespace aliases.

--- main.cpp
+++ main.cpp
@@ -12,26 +12,26 @@
 #include <iostream>
 #include <thread>
 
-namespace logging = boost::log;
+namespace lgg = boost::log;
 namespace src = boost::log::sources;
-namespace keywords = boost::log::keywords;
-namespace trivial = boost::log::trivial;
+namespace kwr = boost::log::keywords;
+namespace trv = boost::log::trivial;
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<trivial::severity_level>)
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<trv::severity_level>)
 
 void initLogging()
 {
-    logging::add_file_log(
-        keywords::file_name = "trace.log",
-        keywords::format = "%TimeStamp% %ThreadID% %Message%");
-    logging::add_common_attributes();
+    lgg::add_file_log(
+        kwr::file_name = "trace.log",
+        kwr::format = "%TimeStamp% %ThreadID% %Message%");
+    lgg::add_common_attributes();
 }
 
 int main(int argc, char** argv)
 {
     initLogging();
-    src::severity_logger_mt<trivial::severity_level>& slg = gLogger::get();
-    BOOST_LOG_SEV(slg, trivial::trace) << "Greetings from the global logger!";
+    src::severity_logger_mt<trv::severity_level>& slg = gLogger::get();
+    BOOST_LOG_SEV(slg, trv::trace) << "Greetings from the global logger!";
 
     unsigned int n = std::thread::hardware_concurrency();
     std::cout << n << " concurrent threads are supported.\n";
