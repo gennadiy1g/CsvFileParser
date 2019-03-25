1c47d80d6807a2d9333192ee09a2f5879b45b819
Gennadiy <gennadiy1g@gmx.com>
2019-03-25 13:53:21 -0400

    Removed headers that are not required to build the project.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-#include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
-#include <boost/log/sources/logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/sources/severity_logger.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/file.hpp>
