e56575addcb34155505f2b30d8b8ae41953637c5
Gennadiy <gennadiy1g@gmx.com>
2019-03-25 16:54:45 -0400

    Replaced textual templates with lambda-style expressions in the formatter.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
+#include <boost/log/attributes/attribute.hpp>
+#include <boost/log/attributes/current_thread_id.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/expressions/formatters/date_time.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/support/date_time.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/file.hpp>
@@ -10,6 +15,7 @@ namespace lgg = boost::log;
 namespace src = boost::log::sources;
 namespace kwr = boost::log::keywords;
 namespace trv = boost::log::trivial;
+namespace expr = boost::log::expressions;
 
 BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<trv::severity_level>)
 
@@ -17,7 +23,10 @@ void initLogging()
 {
     lgg::add_file_log(
         kwr::file_name = "trace.log",
-        kwr::format = "%TimeStamp% %ThreadID% %Message%");
+        kwr::format = (expr::stream
+            << expr::attr<unsigned int>("LineID") << " <" << lgg::trivial::severity << "> "
+            << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", " %Y-%m-%d %H:%M:%S:%f ")
+            << expr::attr<boost::log::thread_id>("ThreadID") << ' ' << expr::message));
     lgg::add_common_attributes();
 }
 
