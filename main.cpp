dd8809e175bda7ddf5454c16e1db63c2ef7e4306
Gennadiy <gennadiy1g@gmx.com>
2019-03-26 15:20:29 -0400

    Added wide character logging.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <thread>
 
+#include <boost/locale/generator.hpp>
 #include <boost/log/attributes/attribute.hpp>
 #include <boost/log/attributes/current_thread_id.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/expressions/formatters/date_time.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/trivial.hpp>
@@ -17,17 +19,20 @@ namespace src = boost::log::sources;
 namespace keyw = boost::log::keywords;
 namespace triv = boost::log::trivial;
 namespace expr = boost::log::expressions;
+namespace sink = boost::log::sinks;
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(GlobalLogger, src::severity_logger_mt<triv::severity_level>)
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(GlobalLogger, src::wseverity_logger_mt<triv::severity_level>)
 
 void initLogging()
 {
-    logg::add_file_log(
+    auto sink = logg::add_file_log(
         keyw::file_name = "trace.log",
         keyw::format = (expr::stream
             << expr::attr<unsigned int>("LineID") << ' ' << triv::severity << ' '
             << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", " %Y-%m-%d %H:%M:%S.%f ")
             << expr::attr<logg::thread_id>("ThreadID") << ' ' << expr::message));
+    std::locale loc = boost::locale::generator()("en_US.UTF-8");
+    sink->imbue(loc);
     logg::add_common_attributes();
 #ifdef NDEBUG
     logg::core::get()->set_filter(triv::severity >= triv::info);
@@ -37,8 +42,9 @@ void initLogging()
 int main(int argc, char** argv)
 {
     initLogging();
-    src::severity_logger_mt<triv::severity_level>& gLogger = GlobalLogger::get();
+    src::wseverity_logger_mt<triv::severity_level>& gLogger = GlobalLogger::get();
     BOOST_LOG_SEV(gLogger, triv::trace) << "->" << FUNCTION_FILE_LINE;
+    BOOST_LOG_SEV(gLogger, triv::trace) << L"Привіт Світ! " << FUNCTION_FILE_LINE;
 
     unsigned int n = std::thread::hardware_concurrency();
     std::cout << n << " concurrent threads are supported.\n";
