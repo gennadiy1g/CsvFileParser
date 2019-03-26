22c86f539e92cc6ab2c86e8499911372304cbc88
Gennadiy <gennadiy1g@gmx.com>
2019-03-26 10:33:11 -0400

    Renamed namespace aliases.

--- main.cpp
+++ main.cpp
@@ -11,30 +11,30 @@
 #include <iostream>
 #include <thread>
 
-namespace lgg = boost::log;
+namespace logg = boost::log;
 namespace src = boost::log::sources;
-namespace kwr = boost::log::keywords;
-namespace trv = boost::log::trivial;
+namespace keyw = boost::log::keywords;
+namespace triv = boost::log::trivial;
 namespace expr = boost::log::expressions;
 
-BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<trv::severity_level>)
+BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(gLogger, src::severity_logger_mt<triv::severity_level>)
 
 void initLogging()
 {
-    lgg::add_file_log(
-        kwr::file_name = "trace.log",
-        kwr::format = (expr::stream
-            << expr::attr<unsigned int>("LineID") << ' ' << lgg::trivial::severity << ' '
+    logg::add_file_log(
+        keyw::file_name = "trace.log",
+        keyw::format = (expr::stream
+            << expr::attr<unsigned int>("LineID") << ' ' << logg::trivial::severity << ' '
             << expr::format_date_time<boost::posix_time::ptime>("TimeStamp", " %Y-%m-%d %H:%M:%S:%f ")
             << expr::attr<boost::log::thread_id>("ThreadID") << ' ' << expr::message));
-    lgg::add_common_attributes();
+    logg::add_common_attributes();
 }
 
 int main(int argc, char** argv)
 {
     initLogging();
-    src::severity_logger_mt<trv::severity_level>& slg = gLogger::get();
-    BOOST_LOG_SEV(slg, trv::trace) << "Greetings from the global logger!";
+    src::severity_logger_mt<triv::severity_level>& slg = gLogger::get();
+    BOOST_LOG_SEV(slg, triv::trace) << "Greetings from the global logger!";
 
     unsigned int n = std::thread::hardware_concurrency();
     std::cout << n << " concurrent threads are supported.\n";
