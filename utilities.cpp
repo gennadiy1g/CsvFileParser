34e8f8eec2c15d132512111827c033bab9f2e536
Gennadiy <gennadiy1g@gmx.com>
2019-05-01 14:08:06 -0400

    Moved initLocalization() and initLogging() into utilities.cpp & utilities.h.
    First successfull compilation.

---
+++ utilities.cpp
@@ -0,0 +1,36 @@
+#include "log.h"
+#include <boost/locale.hpp>
+
+namespace blog = boost::log;
+namespace blkeywords = boost::log::keywords;
+namespace blexpressions = boost::log::expressions;
+namespace blocale = boost::locale;
+
+void initLocalization()
+{
+    // Get global backend, and select winapi backend as default for all categories
+    blocale::localization_backend_manager::global().select("winapi");
+
+    /* Create and install global locale. Non UTF-8 encodings are not supported by winapi backend.
+     * https://www.boost.org/doc/libs/1_69_0/libs/locale/doc/html/using_localization_backends.html */
+    std::locale::global(blocale::generator().generate("en_US.UTF-8"));
+
+    // This is needed to prevent C library to
+    // convert strings to narrow
+    // instead of C++ on some platforms
+    std::ios_base::sync_with_stdio(false);
+}
+
+void initLogging()
+{
+    blog::add_file_log(
+        blkeywords::file_name = "trace.log",
+        blkeywords::format = (blexpressions::stream
+            << blexpressions::attr<unsigned int>("LineID") << ' ' << bltrivial::severity << ' '
+            << blexpressions::format_date_time<boost::posix_time::ptime>("TimeStamp", " %Y-%m-%d %H:%M:%S.%f ")
+            << blexpressions::attr<blog::thread_id>("ThreadID") << ' ' << blexpressions::message));
+    blog::add_common_attributes();
+#ifdef NDEBUG
+    blog::core::get()->set_filter(bltrivial::severity >= bltrivial::info);
+#endif
+}
