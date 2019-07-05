875a08644f3462bbc9c122ac42192938d99967d6
Gennadiy <gennadiy1g@gmx.com>
2019-07-05 16:34:56 -0400

    Introduced class MonetDBBulkLoader.

---
+++ MonetDBBulkLoader.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "BulkLoader.h"
+
+class MonetDBBulkLoader : public BulkLoader {
+
+public:
+    MonetDBBulkLoader() = default; // Constructor
+    virtual ~MonetDBBulkLoader() = default; // Defaulted virtual destructor
+
+    // Disallow assignment and pass-by-value.
+    MonetDBBulkLoader(const MonetDBBulkLoader& src) = delete;
+    MonetDBBulkLoader& operator=(const MonetDBBulkLoader& rhs) = delete;
+
+    // Explicitly default move constructor and move assignment operator.
+    MonetDBBulkLoader(MonetDBBulkLoader&& src) = default;
+    MonetDBBulkLoader& operator=(MonetDBBulkLoader&& rhs) = default;
+};
