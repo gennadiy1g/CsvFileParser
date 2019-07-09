79ed10c6a95495d9dc90036aeac6cf9ba708254c
Gennadiy <gennadiy1g@gmx.com>
2019-07-09 16:41:00 -0400

    WIP on MonetDBBulkLoader::generateCreateTableCommand.

--- MonetDBBulkLoader.cpp
+++ MonetDBBulkLoader.cpp
@@ -16,17 +16,42 @@ std::wstring MonetDBBulkLoader::generateCreateTableCommand() const
 {
     std::wostringstream buf(L"CREATE TABLE ");
     buf << L'"' << boost::trim_copy(mInputFile.stem().wstring()) << L"\" (";
+    auto firstColumn = true;
     for (const auto& column : mParsingResults.columns()) {
+        if (firstColumn) {
+            firstColumn = false;
+        } else {
+            buf << L", ";
+        };
         buf << L'"' << boost::trim_copy(column.name()) << L"\" ";
         switch (column.type()) {
-        case ColumnType::String:;
-        case ColumnType::Float:;
-        case ColumnType::Decimal:;
-        case ColumnType::Int:;
-        case ColumnType::TimeStamp:;
-        case ColumnType::Date:;
-        case ColumnType::Time:;
-        case ColumnType::Bool:;
+        case ColumnType::String:
+            buf << (column.minLength() != column.maxLength() ? L"VARCHAR(" : L"CHAR(") << column.maxLength() << L')';
+        case ColumnType::Float:
+            buf << L"FLOAT(" << column.digitsBeforeDecimalPoint() + column.digitsAfterDecimalPoint() << L")";
+        case ColumnType::Decimal:
+            buf << L"DECIMAL(" << column.digitsBeforeDecimalPoint() + column.digitsAfterDecimalPoint() << L',' << column.digitsAfterDecimalPoint() << L')';
+        case ColumnType::Int: {
+            auto minValue = column.minValue();
+            auto maxValue = column.maxValue();
+            if ((-127 <= minValue) && (maxValue <= 127)) {
+                buf << L"TINYINT";
+            } else if ((-32767 <= minValue) && (maxValue <= 32767)) {
+                buf << L"SMALLINT";
+            } else if ((-2147483647 <= minValue) && (maxValue <= 2147483647)) {
+                buf << L"INT";
+            } else {
+                buf << L"BIGINT";
+            }
+        }
+        case ColumnType::TimeStamp:
+            buf << L"TIMESTAMP";
+        case ColumnType::Date:
+            buf << L"DATE";
+        case ColumnType::Time:
+            buf << L"TIME";
+        case ColumnType::Bool:
+            buf << L"BOOL";
         }
     }
     buf << L')';
