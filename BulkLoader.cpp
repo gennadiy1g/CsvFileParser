7138b01d12d58c04977c7be458da80211e6b2192
Gennadiy <gennadiy1g@gmx.com>
2019-07-12 13:18:08 -0400

    WIP on BulkLoader::load.

--- BulkLoader.cpp
+++ BulkLoader.cpp
@@ -1,4 +1,7 @@
+#include <sstream>
+
 #include "BulkLoader.h"
+#include "nanodbc.h"
 
 BulkLoader::BulkLoader(const bfs::path& inputFile)
     : mInputFile(inputFile)
@@ -13,4 +16,8 @@ void BulkLoader::parse(wchar_t separator, wchar_t quote)
     mParsingResults = parser.parse(separator, quote, L'\\');
 }
 
-void BulkLoader::load(std::string_view table, int port, std::string_view user, std::string_view password) const {}
+void BulkLoader::load(std::string_view table, int port, std::string_view user, std::string_view password) const
+{
+    // DRIVER=MonetDB ODBC Driver;PORT=50000;HOST=<your host>;DATABASE=<your db>;UID=monetdb;PWD=monetdb
+    std::ostringstream buf("DRIVER=MonetDB ODBC Driver;HOST=127.0.0.1;", std::ios_base::ate);
+}
