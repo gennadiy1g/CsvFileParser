ebe32512cb27b6f0b0bf982620c1a985fa5638e3
Gennadiy <gennadiy1g@gmx.com>
2019-07-12 17:17:14 -0400

    WIP on BulkLoader::load.

--- BulkLoader.cpp
+++ BulkLoader.cpp
@@ -1,3 +1,4 @@
+#include <boost/algorithm/string.hpp>
 #include <sstream>
 
 #include "BulkLoader.h"
@@ -19,13 +20,29 @@ void BulkLoader::parse(wchar_t separator, wchar_t quote)
 
 void BulkLoader::load(std::wstring_view table, int port, std::string_view user, std::string_view password) const
 {
+    std::wstring tableTrim(table);
+    boost::trim(tableTrim);
+    if (tableTrim == L"") {
+        tableTrim = boost::trim_copy(mInputFile.stem().wstring());
+    }
+    assert(tableTrim != L"");
+
     auto& gLogger = GlobalLogger::get();
 
     // DRIVER=MonetDB ODBC Driver;PORT=50000;HOST=<your host>;DATABASE=<your db>;UID=monetdb;PWD=monetdb
     std::ostringstream buf("DRIVER=MonetDB ODBC Driver;HOST=127.0.0.1;", std::ios_base::ate);
     buf << "PORT=" << port << ";UID=" << user << ";PWD=" << password << ';';
-    std::string connectionString(buf.str());
+    auto connectionString(buf.str());
     BOOST_LOG_SEV(gLogger, bltrivial::trace) << connectionString;
+
+    auto dropCommand(generateDropTableCommand(tableTrim));
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << dropCommand;
+
+    auto createCommand(generateCreateTableCommand(tableTrim));
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << createCommand;
+
+    auto copyCommand(generateCopyIntoCommand(tableTrim));
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << copyCommand;
 
     nanodbc::connection connection(connectionString);
 }
