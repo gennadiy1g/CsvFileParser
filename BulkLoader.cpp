c7223e2b44868daf1790f6546d9dacc769f75e5b
Gennadiy <gennadiy1g@gmx.com>
2019-07-12 15:24:18 -0400

    Added debug tracing into BulkLoader::load.

--- BulkLoader.cpp
+++ BulkLoader.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 
 #include "BulkLoader.h"
+#include "log.h"
 #include "nanodbc.h"
 
 BulkLoader::BulkLoader(const bfs::path& inputFile)
@@ -18,9 +19,13 @@ void BulkLoader::parse(wchar_t separator, wchar_t quote)
 
 void BulkLoader::load(std::wstring_view table, int port, std::string_view user, std::string_view password) const
 {
-    // DRIVER=MonetDB ODBC Driver;PORT=50000;HOST=<your host>;DATABASE=<your db>;UID=monetdb;PWD=monetdb
-    std::ostringstream connectionString("DRIVER=MonetDB ODBC Driver;HOST=127.0.0.1;", std::ios_base::ate);
-    connectionString << "PORT=" << port << ";UID=" << user << ";PWD=" << password << ';';
+    auto& gLogger = GlobalLogger::get();
 
-    nanodbc::connection connection(connectionString.str());
+    // DRIVER=MonetDB ODBC Driver;PORT=50000;HOST=<your host>;DATABASE=<your db>;UID=monetdb;PWD=monetdb
+    std::ostringstream buf("DRIVER=MonetDB ODBC Driver;HOST=127.0.0.1;", std::ios_base::ate);
+    buf << "PORT=" << port << ";UID=" << user << ";PWD=" << password << ';';
+    std::string connectionString(buf.str());
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << connectionString;
+
+    nanodbc::connection connection(connectionString);
 }
