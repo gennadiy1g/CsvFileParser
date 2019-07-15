faa31efab0555c9d7615e0ae45a05364b062a51e
Gennadiy <gennadiy1g@gmx.com>
2019-07-15 19:01:59 -0400

    Added BOOST_AUTO_TEST_CASE generate_create_table_command_2.

--- main.cpp
+++ main.cpp
@@ -901,4 +901,17 @@ BOOST_AUTO_TEST_CASE(generate_create_table_command_1)
     bulkLoader.load(L"Разбор результатов 1");
 }
 
+BOOST_AUTO_TEST_CASE(generate_create_table_command_2)
+{
+    const std::wstring sourceFile(LR"^(C:\Users\genna_000\Documents\Experiments\test data\parsing_results_2.csv)^");
+    auto& gLogger = GlobalLogger::get();
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << sourceFile << FUNCTION_FILE_LINE;
+    MonetDBBulkLoader bulkLoader(sourceFile);
+    bulkLoader.parse(L',', L'"');
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << bulkLoader.parsingResults().numLines() << L" lines, "
+                                             << bulkLoader.parsingResults().numMalformedLines() << L" malformed lines, "
+                                             << bulkLoader.parsingResults().columns().size() << L" columns" << FUNCTION_FILE_LINE;
+    bulkLoader.load();
+}
+
 BOOST_AUTO_TEST_SUITE_END();
