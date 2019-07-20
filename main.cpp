b8f9ffb3a5a8e5dfc49846b2cb43042aac71325a
Gennadiy <gennadiy1g@gmx.com>
2019-07-19 20:30:23 -0400

    Added BOOST_AUTO_TEST_CASE generate_create_table_command_russian_UTF8.

--- main.cpp
+++ main.cpp
@@ -987,4 +987,17 @@ BOOST_AUTO_TEST_CASE(generate_create_table_command_7)
     bulkLoader.load();
 }
 
+BOOST_AUTO_TEST_CASE(generate_create_table_command_russian_UTF8)
+{
+    const std::wstring sourceFile(LR"^(C:\Users\genna_000\Documents\Experiments\test data\russian_UTF-8.csv)^");
+    auto& gLogger = GlobalLogger::get();
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << sourceFile << FUNCTION_FILE_LINE;
+    MonetDBBulkLoader bulkLoader(sourceFile);
+    bulkLoader.parse(L',', L'"');
+    BOOST_LOG_SEV(gLogger, bltrivial::trace) << bulkLoader.parsingResults().numLines() << L" lines, "
+                                             << bulkLoader.parsingResults().numMalformedLines() << L" malformed lines, "
+                                             << bulkLoader.parsingResults().columns().size() << L" columns" << FUNCTION_FILE_LINE;
+    bulkLoader.load();
+}
+
 BOOST_AUTO_TEST_SUITE_END();
